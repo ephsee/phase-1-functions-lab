6a0904564d4d4496e5701867535035bcef3200d0
Francois Spies <fcspies@gmail.com>
2022-01-10 20:20:42 -0500

    commit tree 7d06a30a358a311962f019c19e49bd4b828b5fc7

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+const blockLength = 264;
+
+function distanceFromHqInBlocks(blocks) {
+    const scuberHQ = 42;
+    const disBlocks = blocks - scuberHQ;
+    if (blocks<42) {
+        return disBlocks*-1
+    } else {
+        return disBlocks
+    }
+}
+distanceFromHqInBlocks()
+
+function distanceFromHqInFeet(blocks) {
+    const disFeet = (distanceFromHqInBlocks(blocks)*blockLength)
+    return disFeet
+}
+distanceFromHqInFeet()
+
+function distanceTravelledInFeet(start, destination) {
+    const trip = (start - destination)*blockLength;
+    if (trip < 0) {
+        return (trip*-1)
+    } else {
+        return trip
+    }
+}
+distanceTravelledInFeet()
+
+function calculatesFarePrice(start, destination) {
+    const fare = distanceTravelledInFeet(start, destination);
+    if (fare > 2500) {
+        return 'cannot travel that far'
+    } else if (fare > 2000) {
+        return 25
+    } else if (fare > 400) {
+        return ((fare*2)-800)/100;
+    } else {
+        return 0
+    }
+}
+calculatesFarePrice()
